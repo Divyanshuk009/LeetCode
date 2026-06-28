e960edf43fad46b12a3d410a3b7b6b8dd6de8b97
Divyanshu Kumar <divyanshuk899@gmail.com>
2026-06-28 15:25:26 +0000

    Sync LeetCode submission - Pow(x, n) (cpp)

---
+++ problems/pow(x,_n)/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+ long long N = n;
+        if(n<0){
+            x=1/x;
+            N=-N;
+        }
+
+        if(N==0){
+            return 1;
+        }
+        if(N==1){
+            return x;
+        }
+    double ans = pow(x, N/ 2);
+
+    if (n % 2 == 0)
+    {
+        return ans * ans;
+    }
+    else
+    {
+        return x * ans * ans;
+    }
+    }
+};
